5566d1703d9481d39f6178e35037e8b773d297e9
sun2ot <sun2ot@qq.com>
2024-08-26 21:26:03 +0800

    comment test&elaluate

--- base/graph_recommender.py
+++ base/graph_recommender.py
@@ -10,15 +10,16 @@ import sys
 
 class GraphRecommender(Recommender):
     def __init__(self, conf, training_set, test_set, **kwargs):
-        super(GraphRecommender, self).__init__(conf, training_set, test_set, **kwargs)
+        super().__init__(conf, training_set, test_set, **kwargs)
         self.data = Interaction(conf, training_set, test_set)
         self.bestPerformance = []
         top = self.ranking['-topN'].split(',')
-        self.topN = [int(num) for num in top]
+        self.topN = [int(num) for num in top]  # 10, 20
         self.max_N = max(self.topN)
 
     def print_model_info(self):
-        super(GraphRecommender, self).print_model_info()
+        """重写父类方法输出模型配置及数据集统计信息"""
+        super().print_model_info()
         # # print dataset statistics
         print('Training Set Size: (user number: %d, item number %d, interaction number: %d)' % (self.data.training_size()))
         print('Test Set Size: (user number: %d, item number %d, interaction number: %d)' % (self.data.test_size()))
@@ -34,38 +35,73 @@ class GraphRecommender(Recommender):
         pass
 
     def test(self):
+        """
+        测试模型
+
+        Returns:
+            rec_list (dict): 推荐列表 `{user: [(item1, score1), (item2, score2), ...]}`
+        """
         def process_bar(num, total):
+            """
+            绘制CLI进度条
+
+            Args:
+                num (int): 当前完成的任务数量
+                total (int): 任务的总数量
+            """
+            # 计算任务完成的比率
             rate = float(num) / total
+            # 根据比率计算需要显示的进度条长度(最长50个字符)
             ratenum = int(50 * rate)
+            # 构造进度条字符串
+            # Progress: [+++++++          ]50%
             r = '\rProgress: [{}{}]{}%'.format('+' * ratenum, ' ' * (50 - ratenum), ratenum*2)
+            # 在命令行中写入进度条，\r用于回到行首，不换行
             sys.stdout.write(r)
+            # 刷新标准输出，确保进度条及时更新
             sys.stdout.flush()
 
         # predict
         rec_list = {}
         user_count = len(self.data.test_set)
         for i, user in enumerate(self.data.test_set):
+            # 生成用户推荐候选
             candidates = self.predict(user)
             # predictedItems = denormalize(predictedItems, self.data.rScale[-1], self.data.rScale[0])
+            #? 这是否意味着评分其实不影响预测结果
             rated_list, li = self.data.user_rated(user)
+            # 根据用户历史评分，排除已评分项目(赋极小值)
+            #? 所以测试集里为什么会存在已评分项目
             for item in rated_list:
                 candidates[self.data.item[item]] = -10e8
+            # 找到评分最高的max_N个物品
             ids, scores = find_k_largest(self.max_N, candidates)
             item_names = [self.data.id2item[iid] for iid in ids]
+            # 存储推荐结果
             rec_list[user] = list(zip(item_names, scores))
+            # 每处理1000个用户，更新进度条
             if i % 1000 == 0:
                 process_bar(i, user_count)
+        # 完成所有用户推荐后，进度条拉到100%(因为正常遍历是无法显示出100%的)
         process_bar(user_count, user_count)
         print('')
         return rec_list
 
     def evaluate(self, rec_list):
+        """
+        评估推荐结果并输出
+
+        Args:
+            rec_list (dict): 推荐列表 `{user: [(item1, score1), (item2, score2), ...]}`
+        """
         self.recOutput.append('userId: recommendations in (itemId, ranking score) pairs, * means the item is hit.\n')
+        # 输出样例
+        # 0: (771,3.333857297897339) (649,3.1552059650421143) (...)
         for user in self.data.test_set:
             line = user + ':'
-            for item in rec_list[user]:
-                line += ' (' + item[0] + ',' + str(item[1]) + ')'
-                if item[0] in self.data.test_set[user]:
+            for item, score in rec_list[user]:
+                line += ' (' + item + ',' + str(score) + ')'
+                if item in self.data.test_set[user]:
                     line += '*'
             line += '\n'
             self.recOutput.append(line)
@@ -75,11 +111,14 @@ class GraphRecommender(Recommender):
         file_name = self.config['model.name'] + '@' + current_time + '-top-' + str(self.max_N) + 'items' + '.txt'
         FileIO.write_file(out_dir, file_name, self.recOutput)
         print('The result has been output to ', abspath(out_dir), '.')
+        # 输出评估指标到文件
         file_name = self.config['model.name'] + '@' + current_time + '-performance' + '.txt'
         self.result = ranking_evaluation(self.data.test_set, rec_list, self.topN)
+        # 日志输出评估指标
         self.model_log.add('###Evaluation Results###')
         self.model_log.add(self.result)
         FileIO.write_file(out_dir, file_name, self.result)
+        # CLI 输出评估指标
         print('The result of %s:\n%s' % (self.model_name, ''.join(self.result)))
 
     def fast_evaluation(self, epoch):
