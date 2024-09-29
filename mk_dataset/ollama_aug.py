025cb62c788a531c3645db73ce744febcf3d2ac6
sun2ot <sun2ot@qq.com>
2024-09-29 08:49:15 +0800

    llm augmentation

---
+++ mk_dataset/ollama_aug.py
@@ -0,0 +1,118 @@
+import sys
+import os
+from os import path
+# 将父目录添加到sys.path中
+root_dir = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.insert(0, root_dir)
+import json
+import time
+import random
+random.seed(666)
+from collections import defaultdict
+from datetime import datetime
+
+from tqdm import tqdm
+import requests
+
+from data.loader import FileIO
+from util.conf import ModelConf
+from util.logger import Log
+
+# 加载配置文件
+config = ModelConf('/home/yzh/code/SELFRec/conf/XSimGCL.yaml')
+host = "http://127.0.0.1:11434/api/generate"
+
+
+def get_response(url, data):
+    """ollama 请求函数"""
+    response = requests.post(url, json=data, timeout=20)
+    response_dict = json.loads(response.text)
+    response_content = response_dict["response"]
+    return response_content
+
+# 初始化日志
+now = datetime.now().strftime('%Y%m%d_%H%M')
+llm_log = Log('llm_aug', f'llm_aug_{now}')
+
+# 加载提示词模板
+template_file = '/home/yzh/code/SELFRec/conf/aug_prompt.txt'
+with open(template_file, 'r') as f:
+    template = f.read()
+
+# 加载训练集
+training_data = FileIO.load_data_set('/home/yzh/code/SELFRec/mk_dataset/yelp_ds_final/train_data.txt',
+                                     config['model']['type'])
+# 加载文本数据
+with open('yelp_out/yelp_text.json', 'r') as text_file:
+    yelp_text = json.load(text_file)
+
+# 初始化双向映射集合
+training_set_u: dict[str, dict[str, str]] = defaultdict(dict)
+for user, item, rating in training_data:
+    training_set_u[user][item] = rating
+
+# 初始化用户历史交互记录
+user_history: dict[str, dict[str, str]] = {}
+
+# 训练集用户
+users = training_set_u.keys()
+
+# 构建用户历史交互数据
+#* sha256: 7fab614c1d970735e7d6e328943ba645ea5c53a2a59ba9aec78667ae256f326e
+file_path = '/nvme0n1p2/yelp_out/yelp_user_history.json'
+if os.path.exists(file_path):
+    llm_log.add('File exists. Skipping user history construction!')
+    with open(file_path, 'r') as file:
+        user_history = json.load(file)
+else:
+    for idx, user in tqdm(enumerate(users), desc='construct user history'):
+
+        items = list(training_set_u.get(user, {}).keys())
+        if len(items) == 0:
+            raise ValueError(f'{user} get no items')
+        # 为保证模型输出效果&效率, 限制上下文数量
+        selected = random.sample(items, min(3, len(items)))
+        his: dict[str, str] = {item: yelp_text[item] for item in selected}
+        user_history[user] = his
+
+    #* 持久化历史交互数据
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(user_history, file, ensure_ascii=False)
+
+
+#* 开始数据增强
+llm_log.add('llm augmentation start...')
+
+# 初始化用户偏好
+#! 为了让代码执行过程可中继，持久化时将采用 json line 的形式，以避免生成单一对象再写入文件
+user_preference: dict[str, str] = {}
+
+user_history = dict(list(user_history.items())[38649:])
+for user, his in tqdm(user_history.items(), desc='llm augment'):
+    try:
+        # 构建 {history} 提示词
+        history_list = [f'{item}: {categories}' for item, categories in his.items()]
+        form_history = '\n'.join(history_list)
+        prompt = template.format(history=form_history)
+
+        # 调用模型
+        data = {
+            "model": "qwen2.5:0.5b",
+            "prompt": prompt,
+            "stream": False
+        }
+        res = get_response(host, data)
+        user_preference[user] = res
+    except Exception as e:
+        llm_log.add(f'{user} error: {e}')
+        user_preference[user] = ''
+    
+    if len(user_preference) % 100 == 0 and len(user_preference) > 0:
+        print(f'processed {len(user_preference)}')
+        #* 持久化用户偏好数据
+        with open(f'/nvme0n1p2/yelp_out/yelp_user_preference-{now}.json', 'a', encoding='utf-8') as file:
+            for user, pref in user_preference.items():
+                file.write(json.dumps({user: pref}, ensure_ascii=False) + '\n')
+        user_preference = {}
+        
+print('finish')
--- util/logger.py
+++ util/logger.py
@@ -13,7 +13,7 @@ class Log(object):
             cls._instance = super(Log, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, module: str = None, filename: str = None):
+    def __init__(self, module: str='', filename: str=''):
         """
         初始化日志模块(单例)self.logger并添加FileHandler
 
@@ -25,22 +25,7 @@ class Log(object):
             self.logger = logging.getLogger(module)
             self.logger.setLevel(level=logging.INFO)
             os.makedirs('./log/', exist_ok=True)
-
-            try:
-                # 仅在第一次创建时生成 filename
-                if Log._filename is None and Log._module is None:
-                    if module and filename:
-                        Log._filename = filename
-                        Log._module = module
-                    else:
-                        raise ValueError('module and filename are required')
-                else:
-                    if filename or module: 
-                        raise ValueError('Log is a singleton, module/filename is not allowed to be changed')
-            except ValueError as e:
-                print(e)
-
-            handler = logging.FileHandler(f'./log/{Log._filename}.log')
+            handler = logging.FileHandler(f'./log/{filename}.log')
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
@@ -53,3 +38,8 @@ class Log(object):
     def catch(exception: Exception, subject: str, position: str = ''):
         """CLI输出自定义异常捕获日志"""
         print(f'\n{"-"*50}\n{position} {subject} error:\n{exception}\n{"-"*50}')
+    
+
+    @staticmethod
+    def cli(positon: str, content: str=''):
+        print(f'[{positon}]: {content}')
