6e93617bf2de1e4c47658b77aae997a503778b71
sun2ot <sun2ot@qq.com>
2024-10-23 20:36:20 +0800

    test ifashion dataset

--- mk_dataset/ollama_aug.py
+++ mk_dataset/ollama_aug.py
@@ -11,25 +11,34 @@ random.seed(666)
 from collections import defaultdict
 from datetime import datetime
 import argparse
+from safetensors.torch import load_file
 
 from tqdm import tqdm
 import requests
-
-from data.loader import FileIO
-from util.conf import ModelConf
 from util.logger import Log
 
 parser = argparse.ArgumentParser(description='LLM augmentation configurations.')
 parser.add_argument('--type', choices=['normal', 'specific'], default='normal', help='augmentation type: normal or specific')
 parser.add_argument('--skip', type=int, default=0, help='Skip the augmentation process based on item index.')
-parser.add_argument('--input', type=str, default='yelp_out/yelp_text.json', help='text file path.')
+parser.add_argument('--text', type=str, default='yelp_out/yelp_text.json', help='text file path.')
+parser.add_argument('--interact', type=str, default='yelp_ds/filter_yelp_interactions.txt', help='interaction dataset path.')
+parser.add_argument('--template', type=str, default='aug_prompt.txt', help='LLM augmentation prompt template.')
+parser.add_argument('--model', type=str, default="qwen2.5:0.5b", help='LLM model.')
+parser.add_argument('--dsname', type=str, default="yelp", help='output file prefix name.')
 parser.add_argument('--output', type=str, default='yelp_out', help='output directory path.')
 args = parser.parse_args()
 
-# 加载配置文件
-config = ModelConf('/home/yzh/code/SELFRec/conf/XSimGCL.yaml')
+"""配置"""
+# ollama
 host = "http://127.0.0.1:11434/api/generate"
 
+# 提示词模板
+with open(args.template, 'r') as f:
+    template = f.read()
+
+# 加载文本数据
+with open(args.text, 'r') as text_file:
+    item_text = json.load(text_file)
 
 def get_response(url, data):
     """ollama 请求函数"""
@@ -42,24 +51,8 @@ def get_response(url, data):
 now = datetime.now().strftime('%Y%m%d_%H%M')
 llm_log = Log(module='llm_aug', filename=f'llm_aug_{now}')
 
-# 加载提示词模板
-template_file = '/home/yzh/code/SELFRec/conf/aug_prompt.txt'
-with open(template_file, 'r') as f:
-    template = f.read()
 
-# 加载训练集
-training_data = FileIO.load_data_set('/home/yzh/code/SELFRec/mk_dataset/yelp_ds_final/train_data.txt',
-                                     config['model']['type'])
-# 加载文本数据
-with open(args.input, 'r') as text_file:
-    yelp_text = json.load(text_file)
-
-# 初始化双向映射集合
-training_set_u: dict[str, dict[str, str]] = defaultdict(dict)
-for user, item, rating in training_data:
-    training_set_u[user][item] = rating
-
-# 初始化用户历史交互记录
+#* 初始化用户历史交互记录
 # {
 #     'user': {
 #         'item1': 'categories1',
@@ -69,28 +62,31 @@ for user, item, rating in training_data:
 # }
 user_history: dict[str, dict[str, str]] = {}
 
-#* 训练集用户
-users = training_set_u.keys()
+#* 全局交互集 user -> set(items)
+interactions: dict[str, set] = defaultdict(set)
+with open(args.interact, 'r') as file:
+    for line in file:
+        user, item = line.split(' ')[0], line.split(' ')[1]
+        interactions[user].add(item)
 
 # 构建用户历史交互数据
-#* sha256: 7fab614c1d970735e7d6e328943ba645ea5c53a2a59ba9aec78667ae256f326e
-if os.path.exists(f"{args.output}/yelp_user_history.json"):
+if os.path.exists(f"{args.output}/{args.dsname}_user_history.json"):
     llm_log.add('File exists. Skipping user history construction!')
-    with open(f"{args.output}/yelp_user_history.json", 'r') as file:
+    with open(f"{args.output}/{args.dsname}_user_history.json", 'r') as file:
         user_history = json.load(file)
 else:
-    for idx, user in tqdm(enumerate(users), desc='construct user history'):
-
-        items = list(training_set_u.get(user, {}).keys())
+    print("'construct user history'")
+    for idx, user in enumerate(interactions):
+        items = list(interactions[user])
         if len(items) == 0:
             raise ValueError(f'{user} get no items')
         # 为保证模型输出效果&效率, 限制上下文数量
         selected = random.sample(items, min(3, len(items)))
-        his: dict[str, str] = {item: yelp_text[item] for item in selected}
-        user_history[user] = his
+        his_dict: dict[str, str] = {item: item_text[item] for item in selected}
+        user_history[user] = his_dict
 
     #* 持久化历史交互数据
-    with open(f"{args.output}/yelp_user_history.json", 'w', encoding='utf-8') as file:
+    with open(f"{args.output}/{args.dsname}_user_history.json", 'w', encoding='utf-8') as file:
         json.dump(user_history, file, ensure_ascii=False)
 
 
@@ -111,9 +107,11 @@ elif args.type == 'specific':
     2. 此处实现逻辑不唯一, 只要能提供需要 specific 的 user_list 即可
     3. 为了不破坏原始增强逻辑, 这里不对原文件进行修改, 而是同样生成新文件
     """
+
+    #* mode1: 处理完成后补全缺失(生成失败)数据
     error_num = 0
     error_users: list[str] = []
-    with open('yelp_user_preferences.v1.json', 'r') as file:
+    with open('ifashion_out/ifashion_user_pref.json', 'r') as file:
         #* 从合并后的文件中找出存在缺失的 user
         pre_data: dict[str, str] = json.load(file)
         for k, v in pre_data.items():
@@ -122,19 +120,29 @@ elif args.type == 'specific':
                 error_users.append(k)
         print(f"total {len(pre_data)} users, {error_num} empty fields")
     user_history = {user: user_history[user] for user in error_users}
+
+    #* mode2: 在已生成的结果中补充缺少的用户
+    # user_pref_embs = load_file("/home/yzh/code/SELFRec/mk_dataset/yelp_ds/user_pre_embs.safetensors")
+    # remain_user = set()
+    # for user in interactions:
+    #     if user in user_pref_embs: continue
+    #     remain_user.add(user)
+    # print(f"remain: {len(remain_user)}")
+    # user_history = {user: user_history[user] for user in remain_user}
+
 else:
     raise ValueError('type error')
 
-for user, his in tqdm(user_history.items(), desc='llm augment'):
+for user, his_dict in tqdm(user_history.items(), desc='llm augment'):
     try:
         # 构建 {history} 提示词
-        history_list = [f'{item}: {categories}' for item, categories in his.items()]
+        history_list = [f'{item}: {categories}' for item, categories in his_dict.items()]
         form_history = '\n'.join(history_list)
         prompt = template.format(history=form_history)
 
         # 调用模型
         data = {
-            "model": "qwen2.5:0.5b",
+            "model": args.model,
             "prompt": prompt,
             "stream": False
         }
@@ -147,14 +155,16 @@ for user, his in tqdm(user_history.items(), desc='llm augment'):
     if len(user_preference) % 100 == 0 and len(user_preference) > 0:
         print(f'processed {len(user_preference)}')
         #* 持久化用户偏好数据
-        with open(f'{args.output}/yelp_user_preference-{now}.json', 'a', encoding='utf-8') as file:
+        with open(f'{args.output}/{args.dsname}_user_preference-{now}.json', 'a', encoding='utf-8') as file:
             for user, pref in user_preference.items():
                 file.write(json.dumps({user: pref}, ensure_ascii=False) + '\n')
-        user_preference = {}
+        user_preference.clear()  # 清空缓存
 
-#* 写入剩余内容
-with open(f'{args.output}/yelp_user_preference-{now}.json', 'a', encoding='utf-8') as file:
-    for user, pref in user_preference.items():
-        file.write(json.dumps({user: pref}, ensure_ascii=False) + '\n')
-        
+
+if len(user_preference) > 0: # 写入剩余内容
+    with open(f'{args.output}/{args.dsname}_user_preference-{now}.json', 'a', encoding='utf-8') as file:
+        for user, pref in user_preference.items():
+            file.write(json.dumps({user: pref}, ensure_ascii=False) + '\n')
+
 print('finish')
+llm_log.add('finish')
